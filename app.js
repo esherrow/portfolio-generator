382b5d9324782041cbd01d6b0ca52d2abb5b48cc
Yoga Cats <esherrow77@gmail.com>
2021-06-01 17:22:26 -0400

    generate html, modularize

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if(err) throw new Error(err);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
-    console.log("==================");
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//const printProfileData = profileDataArr => {
+//    for (let i = 0; i < profileDataArr.length; i++) {
+//      console.log(profileDataArr[i]);
+//    }
+//    console.log("==================");
+
+//    profileDataArr.forEach(profileItem => console.log(profileItem));
         
     
-  };
-printProfileData(profileDataArgs);
+//  };
+//printProfileData(profileDataArgs);

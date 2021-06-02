654db145e2010ba80133223e78ea09dbecc5c397
Yoga Cats <esherrow77@gmail.com>
2021-06-02 14:22:54 -0400

    added questions and recursion

--- app.js
+++ app.js
@@ -3,30 +3,89 @@ const inquirer = require('inquirer');
 // const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
-inquirer
-    .prompt([
+const promptUser = () => {
+
+    return inquirer.prompt([
         {
             type:'input',
             name:'name',
             message:'What is your name?'
+        },
+        {
+            type:'input',
+            name:'github',
+            messag:'Enter your GitHub Username'
+        },
+        {
+            type:'input',
+            name:'about',
+            message:'Provide some information about yourself:'
+        }
+    ]);
+};
+    
+const promptProject = portfolioData => {    
+    if(!portfolioData.projects){
+    portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+          },
+          {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+          },
+          {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+          },
+          {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+          },
+          {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false    
         }
     ])
-    .then(answers => console.log(answers));
-    
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        }else {
+            return portfolioData;
+        }
+    }
+    );
+};
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+
 // fs.writeFile('./index.html', pageHTML, err => {
 //     if(err) throw err;
 
 //     console.log('Portfolio complete! Check out index.html to see the output!')
 // });
-
-//const printProfileData = profileDataArr => {
-//    for (let i = 0; i < profileDataArr.length; i++) {
-//      console.log(profileDataArr[i]);
-//    }
-//    console.log("==================");
-
-//    profileDataArr.forEach(profileItem => console.log(profileItem));
-        
-    
-//  };
-//printProfileData(profileDataArgs);
